6444fe5d9e7c18af0f90f8611bf7aa3ebca0fadb
Kenneth <lopezkenneth2000@yahoo.com>
2021-07-19 20:50:30 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 100) +1;
+
+let num3 = 10;
+let num4 = 2.5;
+const mod = num3 / num4;
+
+let numbers = (0, 5, 15, 10.2403, 20, 19.99999);
+const max = Math.ceil(numbers);
